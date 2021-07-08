914c3e62d4362bd01c92ab99a3dc09e541fd91ea
radibogov <radibogovgleb@gmail.com>
2021-07-08 18:35:11 +0300

    first commit

---
+++ src/app/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import * as ReactDOM from 'react-dom';
+
+
+const App = () => {
+
+
+
+    return (
+        <div>
+            React
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
